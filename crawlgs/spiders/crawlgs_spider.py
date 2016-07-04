ce26970c6cf2b420893dafe2ded8d7c59260f413
LabhanshAgrawal <labhansh.agrawal@gmail.com>
2016-07-04 20:14:51 +0530

    made the crawl independent from number of results in the page

--- crawlgs/spiders/crawlgs_spider.py
+++ crawlgs/spiders/crawlgs_spider.py
@@ -11,14 +11,14 @@ class crawlgsSpider(scrapy.Spider):
     ]
     def parse(self, response):
         item = CrawlgsItem()
-        for x in range(2,12):
-            item['Title'] = "".join(response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/h3/a/text() | //div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/h3/a/b/text()').extract()).encode('utf8')
-            item['URL'] = response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/h3/a/@href').extract()[0].encode('utf8')
-            item['Year'] = re.findall(r'\b\d{4}\b', response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_a"]').extract()[0])[0]
-            item['Citations'] = re.split('</a>', re.split('Cited by ', response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_fl"]').extract()[0])[1])[0]
-            item['Versions'] = re.split('All ', re.split(' version', response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_fl"]').extract()[0])[0])[1]
-            item['Cluster_ID'] = re.split('&', re.split('cites=', response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_fl"]').extract()[0])[1])[0]
-            item['Citations_list'] = 'https://scholar.google.co.in' + response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_fl"]/a[1]/@href').extract()[0]
-            item['Versions_list'] = 'https://scholar.google.co.in' + response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_fl"]/a[@class="gs_nph"][1]/@href').extract()[0]
-            item['Excerpt'] = ''.join(response.xpath('//div[@id="gs_ccl"]/div[' + str(x) + ']/div[@class="gs_ri"]/div[@class="gs_rs"]/text()').extract())
+        for x in response.xpath('/html/body/div[@id="gs_top"]/div[@id="gs_bdy"]/div[@id="gs_res_bdy"]/div[@id="gs_ccl"]/div[@class="gs_r"]/div[@class="gs_ri"]'):
+            item['Title'] = "".join(x.xpath('h3/a/text() | h3/a/b/text()').extract())
+            item['URL'] = x.xpath('h3/a/@href').extract()[0]
+            item['Year'] = re.findall(r'\b\d{4}\b', x.xpath('div[@class="gs_a"]').extract()[0])[0]
+            item['Citations'] = re.split('</a>', re.split('Cited by ', x.xpath('div[@class="gs_fl"]').extract()[0])[1])[0]
+            item['Versions'] = re.split('All ', re.split(' version', x.xpath('div[@class="gs_fl"]').extract()[0])[0])[1]
+            item['Cluster_ID'] = re.split('&', re.split('cites=', x.xpath('div[@class="gs_fl"]').extract()[0])[1])[0]
+            item['Citations_list'] = 'https://scholar.google.co.in' + x.xpath('div[@class="gs_fl"]/a[1]/@href').extract()[0]
+            item['Versions_list'] = 'https://scholar.google.co.in' + x.xpath('div[@class="gs_fl"]/a[@class="gs_nph"][1]/@href').extract()[0]
+            item['Excerpt'] = ''.join(x.xpath('div[@class="gs_rs"]/text()').extract())
             yield item
